373c080b42f42561150a395631dd0f8e89ca3a25
Benoit Montuelle <benoit.montuelle@april.com>
2016-12-16 02:06:52 +0100

    fix(perf) adding output cache

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,54 +1,53 @@
 package controllers
 
 import javax.inject._
-import models.{Country, Runway, Airport}
-import play.api._
-import play.api.mvc._
-import services.{Countries, Airports, Reports}
 
+import models.{Airport, Country, Runway}
+import play.api.cache.Cached
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.mvc._
+import services.{Airports, Countries, Reports}
 
 import scala.concurrent.Future
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+
 @Singleton
-class HomeController @Inject()(reportService: Reports, countries: Countries, airports: Airports) extends Controller {
+class HomeController @Inject()(cached: Cached, reportService: Reports, countries: Countries, airports: Airports) extends Controller {
 
-  /**
-   * Create an Action to render an HTML page with a welcome message.
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index = Action {
-    Ok(views.html.index("Lunatech code assesment demo"))
-  }
-
-  def query(maybeQuery: Option[String]) = Action.async {
-    val fap: Future[Vector[(Airport, Vector[Runway])]] = maybeQuery match {
-      case None => Future.successful(Vector[(Airport, Vector[Runway])]())
-      case Some(q) => {
-        countries.search(q).flatMap((maybeCountry: Option[Country]) => {
-          maybeCountry.map(country => airports.byCountry(country.code))
-            .getOrElse(Future.successful(Vector[(Airport, Vector[Runway])]()))
-        })
-      }
+  def index = cached("index") {
+    Action {
+      Ok(views.html.index("Lunatech code assesment demo"))
     }
-
-    fap.map(ap => Ok(views.html.query(maybeQuery, ap)))
   }
 
-  def reports = Action.async { req =>
-    for {
-      top10Countries <- reportService.top10Countries
-      bottom10Countries <- reportService.bottom10Countries
-      typeOfRunways <- reportService.typeOfRunways
-      top10RunwaysIdent <- reportService.top10RunwaysIdent
-    } yield {
-      Ok(views.html.reports(top10Countries, bottom10Countries, typeOfRunways, top10RunwaysIdent))
+  def query(maybeQuery: Option[String]) = cached(s"query$maybeQuery") {
+    Action.async {
+      val fap: Future[Vector[(Airport, Vector[Runway])]] = maybeQuery match {
+        case None => Future.successful(Vector[(Airport, Vector[Runway])]())
+        case Some(q) => {
+          countries.search(q).flatMap((maybeCountry: Option[Country]) => {
+            maybeCountry.map(country => airports.byCountry(country.code))
+              .getOrElse(Future.successful(Vector[(Airport, Vector[Runway])]()))
+          })
+        }
+      }
+
+      fap.map(ap => Ok(views.html.query(maybeQuery, ap)))
+    }
+  }
+
+  def reports = cached("reports") {
+    Action.async { req =>
+      for {
+        top10Countries <- reportService.top10Countries
+        bottom10Countries <- reportService.bottom10Countries
+        typeOfRunways <- reportService.typeOfRunways
+        top10RunwaysIdent <- reportService.top10RunwaysIdent
+      } yield {
+        Ok(views.html.reports(top10Countries, bottom10Countries, typeOfRunways, top10RunwaysIdent))
+      }
     }
   }
 }
+
+
